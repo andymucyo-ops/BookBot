a40c3ccfea92220f9cc6cf2b5d9f18719404d826
a-nkunzimana <andymucyo@gmail.com>
2026-02-04 00:28:19 +0100

    added doc

--- stats.py
+++ stats.py
@@ -17,7 +17,6 @@ def get_num_words() -> None:
 
 def get_char_freq() -> dict[str,int]:
     #retruns dictionnary with character occurence
-
     full_txt: str = get_book_txt().lower()
 
     char_freq: dict[str,int] = {}
@@ -28,17 +27,15 @@ def get_char_freq() -> dict[str,int]:
             char_freq[char] = 0
         char_freq[char] += 1
 
-
-    # for char in char_freq:
-    #     print(f"- {str(char)}: {char_freq[char]}")
     return char_freq
 
+
 def sort_chars() -> None: 
+    #sorts characters by highest frequency and prints them
     char_freq: dict[str,int] = get_char_freq()
 
     sorted_char_freq: dict[str,int] = dict(sorted(char_freq.items(), key=lambda item: item[1], reverse = True))
    
-    
 
     char_list = [char for char in sorted_char_freq]
     # print(f"char_list: {char_list}")
@@ -50,18 +47,19 @@ def sort_chars() -> None:
     sorted_char_freq_list: list = []
     for i in range(0,len(char_list)):
         sorted_char_freq_list.append(dict(char = char_list[i] , num = num_list[i]))
-
+    # print(sorted_char_freq)
     
+    #exclude non-alphabetical characters from final print
     for i in range(0,len(sorted_char_freq_list)):
         if sorted_char_freq_list[i]['char'].isalpha():
             print(f"'{sorted_char_freq_list[i]['char']}': {sorted_char_freq_list[i]['num']}")
 
 
-    # print(sorted_char_freq)
+    
     
 
 if __name__ == "__main__":
-    #
+    
     # get_book_txt()
     # get_num_words()
     # get_char_freq()
