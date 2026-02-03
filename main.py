fb340ee2d03a98d9cd4e6543d7c9f86dfb19bba8
a-nkunzimana <andymucyo@gmail.com>
2026-02-03 20:35:05 +0100

    added get_num_words function
    and modified main.py structure

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-def main():
-    def get_book_txt():
-        with open("./books/frankenstein.txt", "r") as f:
-            file_content = f.read() 
-            
-            return file_content
 
-    print(get_book_txt())
+def get_book_txt() -> str:
+    with open("./books/frankenstein.txt", "r") as f:
+        file_content = f.read() 
+        
+        return file_content
+
+
+def get_num_words() -> None:
+    list_words: list[str] = get_book_txt().split()
+
+    num_words: int = len(list_words)
+
+    print(f"Found {num_words} total words")
+
+
+
+def main():
+
+    get_book_txt()
+
+    # print(get_book_txt())
+
+    get_num_words()
+
 
 if __name__ == "__main__":
     main()
