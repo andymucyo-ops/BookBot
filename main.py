f74cd505e9e721652504203cca531268d884b74d
a-nkunzimana <andymucyo@gmail.com>
2026-02-03 20:26:12 +0100

    created main function and first run

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    print("Hello from bookbot!")
+    def get_book_txt():
+        with open("./books/frankenstein.txt", "r") as f:
+            file_content = f.read() 
+            
+            return file_content
 
+    print(get_book_txt())
 
 if __name__ == "__main__":
     main()
