58f75cc9e10c4d868eaef36814fbd8da915d5bca
a-nkunzimana <andymucyo@gmail.com>
2026-02-03 21:36:25 +0100

    get_char_freq function added

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 def get_book_txt() -> str:
+    #return whole book content as a string
     with open("./books/frankenstein.txt", "r") as f:
         file_content = f.read() 
         
@@ -6,6 +7,7 @@ def get_book_txt() -> str:
 
 
 def get_num_words() -> None:
+    #prints number of words in the book
     list_words: list[str] = get_book_txt().split()
 
     num_words: int = len(list_words)
@@ -13,4 +15,25 @@ def get_num_words() -> None:
     print(f"Found {num_words} total words")
 
 
+def get_char_freq() -> dict[str,int]:
+    #retruns dictionnary with character occurence
 
+    full_txt: str = get_book_txt().lower()
+    print(full_txt[:5])
+
+    char_freq: dict[str,int] = {}
+
+    for char in full_txt:
+        # print(char)
+        if char not in char_freq:
+            char_freq[char] = 0
+        char_freq[char] += 1
+
+    return char_freq
+
+
+if __name__ == "__main__":
+
+    get_book_txt()
+    get_num_words()
+    print(get_char_freq())
